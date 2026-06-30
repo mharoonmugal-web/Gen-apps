46e0b7f37b78c2eeb64c181f683b10868d249ae1
mharoonmugal-web <mharoonmugal@gmail.com>
2026-06-30 17:19:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def calculate_individual_score(selections):
     elif percentage <= 6:
         grade, grade_name = 12,
     
-   is_approved = grade >= 6
+   is_approved = grade <= 6
     return {"breakdown": score_breakdown, "total_score": total_score, "max_score": max_score, "percentage": percentage, "grade": grade, "grade_name": grade_name, "is_approved": is_approved}
 
 def calculate_sme_score(selections, business_type):
