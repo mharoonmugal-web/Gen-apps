e5f9f7621fe45d08e19c1ccd011289a24c259522
HAROON MIRZA <mharoonmugal@gmail.com>
2026-07-05 01:39:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ def calculate_sme_score(selections, business_type):
 st.markdown("""
 <div class="bank-header">
     <div class="bank-logo">🏦 THE BANK OF PUNJAB</div>
-    <div class="bank-subtitle">Digital Credit Engine - Loan Origitnating & Underwriting System</div>
+    <div class="bank-subtitle">Digital Credit Engine - Loan Originating & Underwriting System</div>
 </div>
 """, unsafe_allow_html=True)
 
