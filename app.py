1044b12808b2c665f74699425913717625fbfb6a
HAROON MIRZA <mharoonmugal@gmail.com>
2026-07-05 21:19:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -643,7 +643,7 @@ if submit_button:
         st.write(f"**Processing Fee:** PKR {processing_fee:,.0f}")
         if product == "Auto Loan":
             st.write(f"**Year 1 Insurance:** PKR {year1_insurance:,.0f}")
-            st.write("Above charges are tentative and final down payment will be calculated at the time of final approval by bank")
+            st.write("Note: Above charges are tentative and final down payment will be calculated at the time of final approval by bank")
     with col2:
       if PRODUCTS[product]["equity"]:
             st.write(f"**Equity Contribution:** PKR {equity_contribution:,.0f}")
